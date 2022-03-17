bc7ed87e0eec85cbfca0366c7ec6766d80b2ccf3
antnikgoo <92442538+antnikgoo@users.noreply.github.com>
2022-03-17 19:42:32 +0300

    Update 17.03.py

--- 17.03.py
+++ 17.03.py
@@ -1,1 +1,43 @@
+1
+=====
+for _ in range(10):
+    print('Python is awesome!')
+    
+2
+=====
+i = input()
+for _ in range(int(input()):
+    print(i)
 
+3
+=====
+for _ in range(6):
+    print('AAA')
+for _ in range(5):
+    print('BBB')
+print('E')
+for _ in range(9):
+    print(T)
+print(G)    
+ 
+4
+=====
+n = int(input())
+if 1 <= n <= 20:
+    for _ in range(n):
+        print('*' * 19)
+               
+5
+=====
+a = input()
+for i in range(10):
+    print(i, a)
+              
+6
+=====
+n = int(input())
+for i in range(n + 1):
+    print('квадрат числа', i, 'равен:', i ** 2)
+
+7
+=====
